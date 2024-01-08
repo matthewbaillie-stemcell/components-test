7b0c4389801fe722740af3beaf0ca1c8a0550575
Matthew Baillie <mbaillie93@gmail.com>
2024-01-08 10:30:35 -0800

    Changes made to style of button componenet for testing

--- dist/components/Button/index.js
+++ dist/components/Button/index.js
@@ -1,11 +1,11 @@
 import "../../assets/index3.css";
 import { jsx as n } from "react/jsx-runtime";
-const u = "_button_lslui_1", r = {
-  button: u
+const r = "_button_s377o_1", u = {
+  button: r
 };
 function b(t) {
   const { className: o, ...s } = t;
-  return /* @__PURE__ */ n("button", { className: `${o} ${r.button}`, ...s });
+  return /* @__PURE__ */ n("button", { className: `${o} ${u.button}`, ...s });
 }
 export {
   b as Button
